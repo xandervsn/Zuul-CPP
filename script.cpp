1cc2a8cdfbf0e994dc80292288db4f1da9379a76
Xander Siruno-Nebel <73612166+xandervsn@users.noreply.github.com>
2022-11-07 14:25:50 -0800

    Update script.cpp

--- script.cpp
+++ script.cpp
@@ -3,6 +3,10 @@
 using namespace std;
 
 int main(){
+  cout << "========================" << endl;
+  cout << "You are NOT the imposter" << endl;
+  cout << "========================" << endl;
 
+  cout << "You are in the CAFETERIA." << endl;
   return 0;
 }
